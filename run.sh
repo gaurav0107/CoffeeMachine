e44f18054bbfe0883e74f4e02582451ddf08bb5a
gaurav0107 <gaurav.dubey2@oyorooms.com>
2020-07-27 02:36:26 +0530

    updating out

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -jar /Users/oyo/worksapce/learn/CoffeeVendingMachine/target/CoffeeVendingMachine-0.0.1-SNAPSHOT.jar input.json
+java -jar target/CoffeeVendingMachine-0.0.1-SNAPSHOT.jar input.json
